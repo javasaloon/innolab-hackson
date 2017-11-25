145e0d8f63f8beab64ced730887304446576f4a0
Jacky Cheng <jacky.cheng@sap.com>
2017-11-25 21:47:13 +0800

    init users

--- hackson/src/main/java/com/innolab/hackson/BlockchainManager.java
+++ hackson/src/main/java/com/innolab/hackson/BlockchainManager.java
@@ -3,30 +3,45 @@ package com.innolab.hackson;
 public class BlockchainManager {
     BlockchainService blockchainService = new BlockchainService();
     BlockchainDao dao = new BlockchainDao();
+    BlockchainAccount tenant = dao.getAccount("tenant");
+    BlockchainAccount app = dao.getAccount("app");
+    BlockchainAccount operator = dao.getAccount("operator");
 
-    public BossAccount createBossAccount() {
-        BlockchainAccount tenant = dao.getAccount("tenant");
-        System.out.println(tenant);
-        BlockchainAccount app = dao.getAccount("app");
-        System.out.println(app);
-        BlockchainAccount operator = dao.getAccount("operator");
-        System.out.println(operator);
+    public BossAccount createBossAccount(boolean isBoss, String name) {
 
         BossAccount bossAccount = new BossAccount();
-        BlockchainAccount user = blockchainService.createUser(app, operator);
-        Card asset = blockchainService.createAsset(user);
-        CardAccount assetAccount = blockchainService.createAssetAccount(user);
 
+        BlockchainAccount user = blockchainService.createUser(app, operator);
         bossAccount.setBlockchainAccount(user);
-        bossAccount.setAsset(asset);
+
+        CardAccount assetAccount = blockchainService.createAssetAccount(user);
         bossAccount.setAssetAccount(assetAccount);
 
-        dao.saveAccount("boss", bossAccount);
+        if (isBoss) {
+            Card asset = blockchainService.createAsset(user);
+            bossAccount.setAsset(asset);
+
+            dao.saveAccount(name, bossAccount);
+        } else {
+            dao.saveAccount(name, bossAccount);
+        }
+
         return bossAccount;
     }
 
+
     public static void main(String[] args) {
         BlockchainManager manager = new BlockchainManager();
-        manager.createBossAccount();
+    }
+
+
+
+    private static void initUsers(BlockchainManager manager) {
+        for (int i = 0; i < 3; i++) {
+            manager.createBossAccount(true, "boss" + i);
+        }
+        for (int i = 0; i < 10; i++) {
+            manager.createBossAccount(false, "player" + i);
+        }
     }
 }
