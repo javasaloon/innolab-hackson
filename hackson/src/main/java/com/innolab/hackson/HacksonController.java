99471b205b3aa3d1fede1d6ffaabc5d647296580
morning <ding222@sjtu.edu.cn>
2017-11-26 02:45:07 +0800

    [ADD] join group

--- hackson/src/main/java/com/innolab/hackson/HacksonController.java
+++ hackson/src/main/java/com/innolab/hackson/HacksonController.java
@@ -19,6 +19,7 @@ public class HacksonController {
     final static String[] bossName = {"boss1", "boss2", "boss3"};
     static Map<Long, List<Battle>> map = new HashMap<>();
     static List<Boss> bossList = new ArrayList<>(bossName.length);
+    Long[] groupId = {-1l, -1l, -1l, -1l, -1l, -1l};
     BlockchainManager blockchainManager = new BlockchainManager();
 
     static {
@@ -58,13 +59,12 @@ public class HacksonController {
 
     @RequestMapping(path = "/players", method = RequestMethod.GET)
     public List<Player> get_userList(@RequestParam(value = "name", defaultValue = "World") String name) {
-        final String[] userName = {"user1", "user2", "user3"};
-        final Long[] userId = {1l, 2l, 3l};
-        final Long[] groupId = {-1l, 1l, 1l};
-        final int[] level = {1, 1, 2};
-        final int[] power = {1000, 1200, 1500};
-        final double[] original_location_x = {-74.0036, -74.01100000000001, -74.00460000000001};
-        final double[] original_location_y = {40.7113, 40.711000000000006, 40.715500000000006};
+        final String[] userName = {"user1", "user2", "user3", "user4", "user5", "user6"};
+        final Long[] userId = {1l, 2l, 3l, 4l, 5l, 6l};
+        final int[] level = {1, 1, 2, 2, 1, 3};
+        final int[] power = {1000, 1000, 1200, 1200, 1000, 1500};
+        final double[] original_location_x = {-74.0036, -74.01100000000001, -74.00460000000001, -73.0036, -73.01100000000001, -73.00460000000001};
+        final double[] original_location_y = {40.7113, 40.711000000000006, 40.715500000000006, 40.7113, 40.711000000000006, 40.715500000000006};
 
         List<Player> userList = new ArrayList<>(userName.length);
         for (int i = 0; i < userName.length; i++) {
@@ -94,8 +94,18 @@ public class HacksonController {
         event_id++;
         int damage = user.getPower();
         double r2 = getRandom(80, 120) / 100.0;
-        System.out.println(r2);
         damage = (int)((double)damage*r2);
+        int multi = 0;
+        int group_id = (int) user.getGroup_id();
+        if(group_id != -1){
+            List<Player> userList = get_userList("World");
+            for(Player p: userList){
+                if(p.getGroup_id() == group_id){
+                    multi++;
+                }
+            }
+            damage = damage*multi;
+        }
         System.out.println(damage);
         user.setPower(damage);
         battle.setDamage(damage);
@@ -140,6 +150,14 @@ public class HacksonController {
         }
         if(start_index < 0) start_index = 0;
         return list.subList(start_index, list.size());
+    }
+
+    @RequestMapping(path = "/join", method = RequestMethod.POST)
+    public Player join_group(@RequestParam(value = "group_id") long group_id, @RequestParam(value = "player_id") long player_id) {
+        Player user = get_userList("World").get(((int) player_id) - 1);
+        user.setGroup_id(group_id);
+        groupId[(int)user.getId()-1] = group_id;
+        return user;
     }
 
     private static double getRandom(int min, int max) {
